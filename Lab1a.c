e1f09a24fcd1b0fc083441aaeed86af3fe7e36e7
EvanSchulte <ejsqm2@mail.missouri.edu>
2016-09-15 10:26:35 -0500

    first version of my code

---
+++ Lab1a.c
@@ -0,0 +1,29 @@
+/*
+ ============================================================================
+ Name        : Lab1.c
+ Author      : Evan Schulte
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define PI 3.14						//stores a value for PI
+
+int main(void) {
+	float area, r;					//variables
+
+	printf("Enter a value for radius > 0: ");	//gets input
+	scanf("%f", &r);
+	while(r<=0){	//if input is less than zero not correct value for radius
+		printf("Invalid input. Enter value for radius >0: ");
+		scanf("%f", &r);//get new input
+	}
+	area = r * r * PI;	//calculates area
+	printf("Area of the circle is: %.2f\n\n", area);	//displays area
+	return EXIT_SUCCESS;
+}
